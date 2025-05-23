8b09ed6caae2f16ae440bf04d87bfe877f23d766
SomethingElse0 <pe3253@my.acm.ac.uk>
2025-05-23 15:59:29 +0100

    item generation over time
    Added function to add collectible items to the map over time. Removed an unnecessary object

---
+++ Assets/GenerateScrap.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateScrap : MonoBehaviour
+{
+    // These are the different items that can be generated over time
+    
+    List<GameObject> generateQueue = new List<GameObject>(); // The items selected to be created are stored in a list 
+    GameObject scrap; // Scrap is used to buy cards in the scrapyard 
+    GameObject coin; // Coins are used to purchace items from
+    GameObject ration; // Rations are used for healing
+    GameObject fuel; // Fuel is used to travel to the next level
+    GameObject levelKey; // Level Keys are used to access the next level
+    // probability of an item being selected, the lower the number, the less likely to drop
+    int scrapCount = 20;
+    int fuelCount = 10;
+    int coinCount = 5;
+    int rationCount = 5;
+    int levelKeyCount = 2;
+    int colliders; // counter for the number of objects actively collided with
+    float counters; // counter for the number of objects to be created
+    bool isInLevel; // is the specified position in the level?
+    void Start()
+    {
+        // identifying what each object is
+        GameObject scrap = transform.GetChild(0).gameObject;
+        GameObject coin = transform.GetChild(1).gameObject;
+        GameObject ration = transform.GetChild(2).gameObject;
+        GameObject fuel = transform.GetChild(3).gameObject;
+        GameObject levelKey = transform.GetChild(4).gameObject;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor")) isInLevel = true;
+        else
+        {
+            colliders++;
+            transform.position = new Vector3(Random.Range(0, 100), Random.Range(0, 100), 0);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor")) isInLevel = false;
+        else
+        {
+            colliders--;
+            transform.position = new Vector3(Random.Range(0, 100), Random.Range(0, 100), 0);
+        }
+    }
+    public void RandomItem(int modifier, int counter)
+    {
+        counters += counter;
+        int totalCount = scrapCount + coinCount + rationCount+fuelCount + modifier;
+        totalCount -= Random.Range(0, totalCount) + scrapCount;
+        while (counters > 0)
+        {
+            if (totalCount < 0) GenerateSpecific(scrap);
+            else
+            {
+                totalCount -= rationCount;
+                if (totalCount < 0) Instantiate(ration, transform.position, transform.rotation);
+                else
+                {
+                    totalCount -= fuelCount;
+                    if (totalCount < 0) Instantiate(fuel, transform.position, transform.rotation);
+                    else
+                    {
+                        totalCount -= levelKeyCount;
+                        if (totalCount < 0) Instantiate(levelKey, transform.position, transform.rotation);
+                        else Instantiate(coin, transform.position, transform.rotation);
+                    }
+                }
+            }
+            counters--;
+        }
+    }
+    public void GenerateSpecific(GameObject item)
+    {
+        generateQueue.Add(item);
+        while (generateQueue.Count>0)
+        {
+            if (isInLevel && colliders == 0)
+            {
+                Instantiate(generateQueue[0]);
+                generateQueue.RemoveAt(0);
+            }
+        }
+    }
+}
