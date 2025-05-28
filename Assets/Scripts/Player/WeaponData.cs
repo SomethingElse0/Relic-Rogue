59913572103150081f01eb1239c97c7a5fd153e4
SomethingElse0 <pe3253@my.acm.ac.uk>
2025-05-28 12:03:19 +0100

    linking gun to scriptable object

---
+++ Assets/Scripts/Player/WeaponData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "GunData", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+public class WeaponData : ScriptableObject
+{
+    public int maxAmmo;
+    public float attackCooldown;
+    public float reloadTime;
+    public int damage;
+    public int bulletBounces;
+    public bool bulletsCollectable;
+    public bool isMelee;
+}
--- Assets/Scripts/Player/WeaponSystem.cs
+++ Assets/Scripts/Player/WeaponSystem.cs
@@ -1,20 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class WeaponSystem : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    InputActionAsset actions;
+    Vector3 direction;
+    float oldDirection=1;
+    int ammo;//I will need to go back and change this out to get ammo to replenish automatically
+    GameObject bullet;
+    float timeSinceLastReload;
+    float timeOfLastAttack;
+    WeaponData GunData;
+
+    void Update()
     {
-        
+        if (Input.mousePresent) direction = Camera.main.ScreenToWorldPoint(Vector3.Normalize(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z)));
+        else direction = actions.FindActionMap("Movement").FindAction("movement").ReadValue<Vector2>();
+        if (direction.x * oldDirection < 0)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z); 
+            oldDirection = direction.x;
+        }
+        transform.LookAt(direction);
     }
 
     // Update is called once per frame
-    void Attack()
+    public void Attack()
     {
-        /* if (equipped && isRanged && ammo > 0) RangedAttack();
-         * 
-         */
+        if (ammo > 0&&Time.time>timeSinceLastReload + GunData.reloadTime &&Time.time>timeOfLastAttack)
+        {
+            Instantiate(bullet, transform.position + direction, transform.rotation);
+            ammo--;
+            timeOfLastAttack = Time.time + GunData.attackCooldown;
+        }
+    }
+    void Reload()
+    {
+        timeSinceLastReload = Time.time;
+        ammo = GunData.maxAmmo;
+    }
+    void ChangeWeapon(ScriptableObject newWeapon)
+    {
+        // set variables to newWeapon Variables including: Ammo, bullet, reload time, attack cooldown, max ammo, gun model
+        Reload();
     }
 }
