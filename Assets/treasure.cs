c26204c79c84d62c8f32d76587f6c63b79a7569b
SomethingElse0 <pe3253@my.acm.ac.uk>
2025-06-02 12:27:42 +0100

    reworking treasure card to not require a separate Game Object
     unity now facing an infinite loop???

--- Assets/treasure.cs
+++ Assets/treasure.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class treasure : MonoBehaviour
 {
     // Start is called before the first frame update
+    Transform generator;
     void Awake()
     {
-        transform.parent.GetChild(0).SendMessage("GenerateSpecific", "coin");
-        transform.parent.GetChild(0).SendMessage("GenerateSpecific", "levelKey");
+        generator= transform.parent.GetChild(0);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<GenerateScrap>().GenerateSpecific(transform.GetChild(1).gameObject);
+        GetComponent<GenerateScrap>().GenerateSpecific(transform.GetChild(4).gameObject);
+        Destroy(this);
     }
 }
